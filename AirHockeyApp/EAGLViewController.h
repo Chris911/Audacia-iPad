6b992cd6b27b5380b5ba1dd676645fccc96e04f9
Chris911 <christophe.naud.dulude@gmail.com>
2013-01-09 16:35:59 +0000

    refs #1045 @01h00 &Commit initial du projet iPad&

---
+++ AirHockeyApp/EAGLViewController.h
@@ -0,0 +1,43 @@
+//
+//  EAGLViewController.h
+//  AppDemo
+//
+
+#import <UIKit/UIKit.h>
+
+#import <OpenGLES/EAGL.h>
+
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#import "OpenGLWaveFrontObject.h"
+
+extern float const LARGEUR_FENETRE;
+extern float const HAUTEUR_FENETRE;
+
+@interface EAGLViewController : UIViewController <UIGestureRecognizerDelegate> {
+    EAGLContext *context;
+    GLuint program;
+    
+    BOOL animating;
+    NSInteger animationFrameInterval;
+    CADisplayLink *displayLink;
+    
+    OpenGLWaveFrontObject *cube;
+}
+
+@property (readonly, nonatomic, getter=isAnimating) BOOL animating;
+@property (nonatomic) NSInteger animationFrameInterval;
+@property (nonatomic, retain) OpenGLWaveFrontObject *cube;
+@property (retain, nonatomic) IBOutlet UIView *LeftSideView;
+
+- (void)startAnimation;
+- (void)stopAnimation;
+- (void)setupView;
+- (void)rotationDetectee:(UIGestureRecognizer *)gestureRecognizer;
+
+- (IBAction)OpenLeftSideView:(id)sender;
+
+
+@end

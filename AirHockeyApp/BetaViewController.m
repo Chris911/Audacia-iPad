9106dab43078b8dee47633fffa9e869630ac5da7
sadesb <sadesb@81f6ee31-9eaa-4ef0-b3cf-fdda7bff9a6b>
2013-01-15 15:59:29 +0000

    refs #1176 @00h30 &Singleton et autres modifications mineurs&

--- AirHockeyApp/BetaViewController.m
+++ AirHockeyApp/BetaViewController.m
@@ -37,6 +37,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft
+            || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
+}
+
 - (void)dealloc {
     [super dealloc];
 }
--- AirHockeyApp/Scene.m
+++ AirHockeyApp/Scene.m
@@ -5,9 +5,24 @@
 //  Created by Chris on 13-01-14.
 //
 //  DESC: Interface between GL view and backend model (rendering tree)
+//  Singleton Class
 
 #import "Scene.h"
+#import "RenderingTree.h"
 
 @implementation Scene
+
+static RenderingTree *renderingTree;
+
+// Singleton of a rendering tree
++ (void)initialize
+{
+    static BOOL initialized = NO;
+    if(!initialized)
+    {
+        initialized = YES;
+        renderingTree = [[[RenderingTree alloc]init]autorelease];
+    }
+}
 
 @end

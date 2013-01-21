b3687ed91e02ad3ef4cf06171dac42712d43cb84
chnau <chnau@81f6ee31-9eaa-4ef0-b3cf-fdda7bff9a6b>
2013-01-21 04:46:56 +0000

    refs #1232 @04h00 &Implementation du WebClient. Screenshot de l'environnement GL et envoie au serveur&

---
+++ AirHockeyApp/WebClient.h
@@ -0,0 +1,24 @@
+//
+//  WebClient.h
+//  AirHockeyApp
+//
+//  Created by Chris on 13-01-20.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "AFHTTPClient.h"
+
+//NSString* const defaultServer = @"http://kepler.step.polymtl.ca/";
+//NSString* const defaultUploadScript = @"/projet3/scripts/upload.php";
+
+@interface WebClient : NSObject
+
+@property (retain,nonatomic) AFHTTPClient* AFClient;
+@property (retain,nonatomic) NSString* server;
+@property (retain,nonatomic) NSString* uploadScript;
+
+- (id) initWithDefaultServer;
+- (void) uploadMapData:(NSString*)mapName :(UIImage*)mapImage;
+
+@end

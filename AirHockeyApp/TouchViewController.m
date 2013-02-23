bd7c0f535126f024b71e00ac16b44c5146e0ee4c
sadesb <sadesb@81f6ee31-9eaa-4ef0-b3cf-fdda7bff9a6b>
2013-02-23 01:22:12 +0000

    calvasse

---
+++ AirHockeyApp/TouchViewController.m
@@ -0,0 +1,38 @@
+//
+//  TouchViewController.m
+//  AirHockeyApp
+//
+//  Created by Sam DesRochers on 2013-02-20.
+//
+//
+
+#import "TouchViewController.h"
+
+@interface TouchViewController ()
+
+@end
+
+@implementation TouchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
